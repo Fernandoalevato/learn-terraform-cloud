8baefa3870961689551404fe63b079ba969b41af
Fernandoalevato <fe.0@hotmail.com>
2022-11-29 16:42:39 -0300

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,7 @@
 terraform {
-
+/*
   cloud {
-    organization = "organization-name"
+    organization = "Alevato_Cloud"
 
     workspaces {
       name = "learn-terraform-cloud"
@@ -14,6 +14,7 @@ terraform {
       version = "~> 3.28.0"
     }
   }
+*/
 
   required_version = ">= 0.14.0"
 }
